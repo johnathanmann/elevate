3a32f70c4b365bba0c03c404d12f3456b5307a7c
Johnny <johnathan.mnn@gmail.com>
2023-08-10 17:38:16 -0500

    React router working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import {
+  HashRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Nav from "./layouts/nav";
+import Home from "./pages/home";
+import Menu from "./pages/menu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <HashRouter>
+    <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />}/>
+      </Routes>
+    </HashRouter>
+  </div>
   );
 }
 
