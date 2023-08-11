a981a8d079f5b836d76f897444d079ab4c9a7994
Johnny <johnathan.mnn@gmail.com>
2023-08-10 19:52:40 -0500

    Nav menu slide open working

--- src/layouts/nav.js
+++ src/layouts/nav.js
@@ -1,7 +1,29 @@
 import hamburger from "../assets/images/elevate-hamburger.png";
 import "../assets/styles/nav.css"
 export default function Nav(){
+
+    const openMenu = (e) => {
+        document.getElementById("nav-menu").classList.add("slide-over")
+    }
+
+    const closeMenu = (e) => {
+        document.getElementById("nav-menu").classList.add("slide-back")
+        document.getElementById("nav-menu").classList.remove("slide-over")
+    }
+
     return(
-        <nav id="nav" className="container-fluid"><span className="bely-display"><h1>Elevate</h1></span> <img src={hamburger}alt="Hamburger icon, three small horizontal bars indicating a menu"/></nav>
+        <div id="nav">
+            <nav id="nav" className="container-fluid">
+                <h1 className="bely-display">Elevate</h1>
+                <img onClick={() => openMenu()} src={hamburger}alt="Hamburger icon, three small horizontal bars indicating a menu"/>
+            </nav>
+            <div id="nav-menu">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
     )
 }
