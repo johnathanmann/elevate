d68d1d4c76ce79fe48e018d2d6ef8495c781489b
Johnny <johnathan.mnn@gmail.com>
2023-08-12 16:56:50 -0500

    Homepage header styled

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,14 @@
+import cafe from "../assets/images/cafe.jpg";
+import "../assets/styles/home.css";
 export default function Homepage(){
     return(
-        <div>
-            <h1>Homepage</h1>
-        </div>
+        <main className="container-fluid" id="homepage">
+            <header className="text-center">
+                <h1 className="header bely-display black">Elevate</h1>
+                <div id="contain">
+                <img className="img-fluid" src={cafe} />
+                </div>
+            </header>
+        </main>
     )
 }
