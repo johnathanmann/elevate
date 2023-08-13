68eea47c3e7cfd04e9ad15b49f109a11ab60374c
Johnny <johnathan.mnn@gmail.com>
2023-08-12 21:20:53 -0500

    Adjusted media queires for homepage header

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,18 @@ import cafe from "../assets/images/cafe.jpg";
 import "../assets/styles/home.css";
 export default function Homepage(){
     return(
-        <main className="container-fluid" id="homepage">
-            <header className="text-center">
+        <div id="homepage">
+            <section className="container-fluid">
+            <header className="row text-center">
                 <h1 className="header bely-display black">Elevate</h1>
-                <div id="contain">
-                <img className="img-fluid" src={cafe} />
-                </div>
+                <figure id="contain">
+                    <img className="img-fluid" src={cafe} />
+                </figure>
             </header>
-        </main>
+            </section>
+            <section className="container-fluid text-center" id="seperator">
+                <p>Good Coffee ~ Good Times ~ Good People </p>
+            </section>
+        </div>
     )
 }
