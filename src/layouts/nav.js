8632b264c4acc11abf213c9786f3309cb4bfc90a
Johnny <johnathan.mnn@gmail.com>
2023-08-31 13:00:26 -0500

    Hamburger fades on menu open and close

--- src/layouts/nav.js
+++ src/layouts/nav.js
@@ -6,18 +6,22 @@ export default function Nav(){
     const openMenu = (e) => {
         document.getElementById("nav-menu").classList.add("slide-over")
         document.getElementById("nav-menu").classList.remove("slide-back")
+        document.getElementById("hamburger").classList.add("remove-hamburger")
+        document.getElementById("hamburger").classList.remove("add-hamburger")
     }
 
     const closeMenu = (e) => {
         document.getElementById("nav-menu").classList.add("slide-back")
         document.getElementById("nav-menu").classList.remove("slide-over")
+        document.getElementById("hamburger").classList.remove("remove-hamburger")
+        document.getElementById("hamburger").classList.add("add-hamburger")
     }
 
     return(
         <div id="nav">
             <nav id="nav" className="container-fluid">
                 <h1 className="bely-display">Elevate</h1>
-                <img onClick={() => openMenu()} src={hamburger}alt="Hamburger icon, three small horizontal bars indicating a menu"/>
+                <img id="hamburger"onClick={() => openMenu()} src={hamburger}alt="Hamburger icon, three small horizontal bars indicating a menu"/>
             </nav>
             <div id="nav-menu">
                 <header>
