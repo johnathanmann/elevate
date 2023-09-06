f031a508457b076b324485c6159822793ad82180
Johnny <johnathan.mnn@gmail.com>
2023-09-06 10:32:19 -0500

    Menu page styling started img changes based on list item hover

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,20 +1,35 @@
+import { useState, useEffect } from "react";
 import "../assets/styles/menu.css";
+import sign from "../assets/images/elevate-sign.png";
+import coffee from "../assets/images/coffee.jpg";
+import teas from "../assets/images/tea.jpg";
+import espresso from "../assets/images/espresso.jpg";
+import croissant from "../assets/images/croissant.jpg";
 
 export default function Menu(){
-    let activeImg;
+    let [activeImg, setActiveImg] =useState(sign);
+
+    function changeImg(img){
+        setActiveImg(img)
+    }
+
+    function resetImg(){
+        setActiveImg(sign)
+    }
+
     return(
-        <main id="menu">
+        <main id="menu" className="container-fluid">
             <section className="row">
-                <article className="col-md-6">
+                <article className="col-md-6 col-sm-12">
                     <ul>
-                        <li>Coffee</li>
-                        <li>Espresso</li>
-                        <li>Other</li>
-                        <li>Food</li>
+                        <li onMouseOver={() => changeImg(coffee)} onMouseLeave={() => resetImg()} className="bely-display display-1 black">Coffee</li>
+                        <li onMouseOver={() => changeImg(espresso)} onMouseLeave={() => resetImg()} className="bely-display display-1 black">Espresso</li>
+                        <li onMouseOver={() => changeImg(teas)} onMouseLeave={() => resetImg()} className="bely-display display-1 black">Teas</li>
+                        <li onMouseOver={() => changeImg(croissant)} onMouseLeave={() => resetImg()} className="bely-display display-1 black">Eats</li>
                     </ul>
                 </article>
-                <figure className="col-md-6">
-                    <img src={activeImg} />
+                <figure className="col-md-6 col-sm-12">
+                    <img className="img-fluid" id="menu-img" src={activeImg} />
                 </figure>
             </section>
         </main>
