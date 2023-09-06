130443df40a38a9925fdf1ca479b00b691a2b018
Johnny <johnathan.mnn@gmail.com>
2023-09-06 10:11:38 -0500

    Added images adjusted footer and nav sizings

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,7 +1,22 @@
+import "../assets/styles/menu.css";
+
 export default function Menu(){
+    let activeImg;
     return(
-        <div>
-            <h1>Menu</h1>
-        </div>
+        <main id="menu">
+            <section className="row">
+                <article className="col-md-6">
+                    <ul>
+                        <li>Coffee</li>
+                        <li>Espresso</li>
+                        <li>Other</li>
+                        <li>Food</li>
+                    </ul>
+                </article>
+                <figure className="col-md-6">
+                    <img src={activeImg} />
+                </figure>
+            </section>
+        </main>
     )
 }
